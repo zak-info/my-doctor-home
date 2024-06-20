7478c1050e83a5b1c0fc7cabadcc69cc95121955
zak-info <z.benslite@esi-sba.dz>
2024-06-20 14:45:14 +0100

    second commit 3

--- components/Doctors/Racent.jsx
+++ components/Doctors/Racent.jsx
@@ -11,7 +11,7 @@ const Racent = () => {
                     <span className='text-xs '>Dr. Marcus</span>
                 </div>
                 <div className='flex flex-col items-center'>
-                    <Image src={"/images/p5.svg"} width={100} height={100} className='w-12 h-12 rounded-full' />
+                    <Image src={"/images/p4.svg"} width={100} height={100} className='w-12 h-12 rounded-full' />
                     <span className='text-xs '>Dr. Maria</span>
                 </div>
                 <div className='flex flex-col items-center'>
